55c4e802bb064ab45228ceabed8d787c2a84eb50
Louis_45 <github@louis45.de>
2024-04-07 14:17:36 +0200

    Init from 18a726e of MelonLoader

---
+++ Dependencies/SupportModules/Preload/Preload.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+namespace MelonLoader.Support
+{
+    internal static class Preload
+    {
+        private static void Initialize()
+        {
+            string managedFolder = string.Copy(GetManagedDirectory());
+
+            string systemPath = Path.Combine(managedFolder, "System.dll");
+            if (!File.Exists(systemPath))
+                WriteResource("System.dll", systemPath);
+
+            string systemCorePath = Path.Combine(managedFolder, "System.Core.dll");
+            if (!File.Exists(systemCorePath))
+                WriteResource("System.Core.dll", systemCorePath);
+
+            string systemDrawingPath = Path.Combine(managedFolder, "System.Drawing.dll");
+            if (!File.Exists(systemDrawingPath))
+                WriteResource("System.Drawing.dll", systemCorePath);
+        }
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        private static extern string GetManagedDirectory();
+
+        private static void WriteResource(string name, string destination)
+        {
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(name);
+            var fileStream = new FileStream(destination, FileMode.CreateNew);
+            for (int i = 0; i < stream!.Length; i++)
+                fileStream.WriteByte((byte)stream.ReadByte());
+            fileStream.Close();
+        }
+    }
+}
