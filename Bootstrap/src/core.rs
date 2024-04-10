1b44d7cbd9051c095a4af394df4c02f5500c6456
NONE <none@valvesoftware.cc>
2024-04-10 06:49:38 +0200

    Update core.rs

--- Bootstrap/src/core.rs
+++ Bootstrap/src/core.rs
@@ -6,10 +6,6 @@ use crate::{console, errors::DynErr, hooks, internal_failure, logging::logger};
 
 #[ctor]
 fn startup() {
-    if !is_vrchat_process() {
-        return;
-    }
-
     init().unwrap_or_else(|e| {
         internal_failure!("Failed to initialize MelonLoader: {}", e.to_string());
     })
@@ -18,7 +14,18 @@ fn startup() {
 fn init() -> Result<(), DynErr> {
     console::init()?;
     println!("Health Check");
+    let current_exe = std::env::current_exe()?;
+    let game_name = current_exe
+        .file_name()
+        .ok_or("Failed to get game name")?
+        .to_str()
+        .ok_or("Failed to get game name")?;
 
+    println!("Game Name: {}", game_name);
+
+    if !game_name.starts_with("VRChat") {
+        return Ok(());
+    }
     logger::init()?;
 
     hooks::init_hook::hook()?;
@@ -31,7 +38,7 @@ fn init() -> Result<(), DynErr> {
 fn is_vrchat_process() -> bool {
     std::env::current_exe()
         .ok()
-        .and_then(|path| path.file_name().map(|name| name.to_string_lossy().starts_with("VRChat")))
+        .and_then(|path| path.file_name().map(|name| name.to_string_lossy().starts_with("V")))
         .unwrap_or(false)
 }
 
